de6f4958aded16b5af269f32a4f16f981b4589df
ultimathul3 <vadim182002@gmail.com>
2020-07-06 20:37:51 +0300

    v. 1.1 (bug fix)

--- src/load_template.cpp
+++ src/load_template.cpp
@@ -17,7 +17,7 @@ void load_template(string name)
 		world.assign(2,  vector2d(N, vector<int>(M, 0)));
 		
 		cell.setSize(Vector2f(cells_size, cells_size));
-		delete window;
+		window->close();
 		window = new RenderWindow(VideoMode(width, height), "Game of Live");
 	}
 
